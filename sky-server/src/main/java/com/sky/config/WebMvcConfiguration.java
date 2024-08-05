16407c425551248f612b47194aea0ad40ef76ab8
31860 <3186055209@qq.com>
2024-08-05 16:56:43 +0800

    add Employee

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -65,4 +65,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry){
+//        registry.addResourceHandler()
+//    }
+//
 }
